c9c80d60082909e4b7dd16720770687bce0d77db
Utry <uka793@mail.ru>
2013-05-11 13:22:24 +0400

    Sorry, delete file
    Sorry, delete file

--- block/header.php
+++ block/header.php
@@ -1,1 +1,11 @@
-﻿
+﻿<div class="hmenu_back">
+	<div class="hmenu">
+
+   		<ul>
+			<li><a href="" >Ссылка1</a></li>
+			<li><a href="" >Ссылка2</a></li>
+			<li><a href="" >Ссылка3</a></li>
+   		</ul>
+     
+	</div>
+</div>

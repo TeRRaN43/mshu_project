4cca4c66c1108f53c936a38391ee138cf6585062
Utry <uka793@mail.ru>
2013-05-11 13:15:18 +0400

    leftMenu
    update file

--- block/lefttd.php
+++ block/lefttd.php
@@ -1,10 +1,18 @@
 ﻿<div class="blok">
         	<div class="blok_zag">Направления</div>
             <div class="blok_con">
-            Консультационная:<br>
-            <ul>
-				<li><a href="?list=teachers" onClick="vote('teachers','','content'); return false">Преподавателям</a></li>
-			</ul>
+                  
+<ul id="navmenu-v">
+<li><a href="">Консультационная</a>
+	<ul>
+		<li><a href="?list=teachers" onClick="vote('teachers','','content'); return false">Преподавателям</a></li>
+		<li><a href="">Студенту</a></li>
+	</ul>
+</li>
+<li><a href="">Научная</a></li>
+<li><a href="">Учебная</a></li>
+</ul>
+            
             
             </div>
             </div>
